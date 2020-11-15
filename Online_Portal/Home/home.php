538b559d2d11e645bfddb2f3d55087225c01af65
SuraManasa <74276669+SuraManasa@users.noreply.github.com>
2020-11-14 23:26:14 -0500

    Add files via upload

---
+++ Online_Portal/Home/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../template/template.php'; ?>
+    <div class="container">
+        <h4>
+            </br>
+            The Volunteer Connection
+        </h4>
+        <h8> &nbsp; &nbsp; &nbsp; online Volunteering Portal</h8>
+    </div>
+
+</head>
+<body>
+    
+</body>
+</html>
+
---
+++ Online_Portal/template/template.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <link href="../css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="page-header" style="height:60px;">
+        <h1></h1>
+    </div>
+    <div class="bs-component">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><h5> <strong>Home</strong></h5>
+                </a>
+            </li>
+        </div>
+        </nav> 
+    </div>      
+</div>
+</body>
+</html>
+
+
